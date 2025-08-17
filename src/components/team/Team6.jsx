901f9710aed654e0b1651ebf6829a0497a7e1874
顾天浩 <gutianhao@MacBook-Air-3.local>
2025-08-17 18:31:22 +0200

    final

--- src/components/team/Team6.jsx
+++ src/components/team/Team6.jsx
@@ -8,14 +8,14 @@ export function Team6() {
   return (
     <section className="w-full py-16 md:py-24 lg:py-28">
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto mb-12 max-w-lg text-center md:mb-18 lg:mb-20">
+        <div className="mb-12 max-w-lg md:mb-18 lg:mb-20">
           <h2 className="mb-5 text-4xl font-bold md:mb-6 md:text-5xl">
             Research Assistant
           </h2>
         </div>
-        <div className="grid grid-cols-1 items-start justify-center gap-x-8 gap-y-12 md:grid-cols-3 md:gap-x-8 md:gap-y-16 lg:gap-x-12">
-          <div className="flex flex-col text-center">
-            <div className="mb-5 flex w-full items-center justify-center md:mb-6">
+        <div className="grid grid-cols-1 items-start gap-x-8 gap-y-12 md:grid-cols-3 md:gap-x-8 md:gap-y-16 lg:gap-x-12">
+          <div className="flex flex-col items-start">
+            <div className="mb-5 md:mb-6">
               <img
                 src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg"
                 alt="Relume placeholder image 1"
@@ -30,7 +30,7 @@ export function Team6() {
               Passionate about private equity, he leads innovative research
               initiatives.
             </p>
-            <div className="mt-6 grid grid-flow-col grid-cols-[max-content] gap-3.5 self-center">
+            <div className="mt-6 flex gap-3.5">
               <a href="#">
                 <LinkedinLogo className="size-6 text-text-primary" />
               </a>
@@ -42,8 +42,8 @@ export function Team6() {
               </a>
             </div>
           </div>
-          <div className="flex flex-col text-center">
-            <div className="mb-5 flex w-full items-center justify-center md:mb-6">
+          <div className="flex flex-col items-start">
+            <div className="mb-5 md:mb-6">
               <img
                 src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg"
                 alt="Relume placeholder image 1"
@@ -58,7 +58,7 @@ export function Team6() {
               Specialises in data-driven insights to enhance investment
               strategies.
             </p>
-            <div className="mt-6 grid grid-flow-col grid-cols-[max-content] gap-3.5 self-center">
+            <div className="mt-6 flex gap-3.5">
               <a href="#">
                 <LinkedinLogo className="size-6 text-text-primary" />
               </a>
@@ -70,8 +70,8 @@ export function Team6() {
               </a>
             </div>
           </div>
-          <div className="flex flex-col text-center">
-            <div className="mb-5 flex w-full items-center justify-center md:mb-6">
+          <div className="flex flex-col items-start">
+            <div className="mb-5 md:mb-6">
               <img
                 src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg"
                 alt="Relume placeholder image 1"
@@ -83,7 +83,7 @@ export function Team6() {
               <h6 className="text-lg">Project Manager</h6>
             </div>
             <p>Coordinates projects to ensure timely and effective delivery.</p>
-            <div className="mt-6 grid grid-flow-col grid-cols-[max-content] gap-3.5 self-center">
+            <div className="mt-6 flex gap-3.5">
               <a href="#">
                 <LinkedinLogo className="size-6 text-text-primary" />
               </a>
@@ -95,8 +95,8 @@ export function Team6() {
               </a>
             </div>
           </div>
-          <div className="flex flex-col text-center">
-            <div className="mb-5 flex w-full items-center justify-center md:mb-6">
+          <div className="flex flex-col items-start">
+            <div className="mb-5 md:mb-6">
               <img
                 src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg"
                 alt="Relume placeholder image 1"
@@ -111,7 +111,7 @@ export function Team6() {
               Supports the team with comprehensive literature reviews and data
               collection.
             </p>
-            <div className="mt-6 grid grid-flow-col grid-cols-[max-content] gap-3.5 self-center">
+            <div className="mt-6 flex gap-3.5">
               <a href="#">
                 <LinkedinLogo className="size-6 text-text-primary" />
               </a>
@@ -123,8 +123,8 @@ export function Team6() {
               </a>
             </div>
           </div>
-          <div className="flex flex-col text-center">
-            <div className="mb-5 flex w-full items-center justify-center md:mb-6">
+          <div className="flex flex-col items-start">
+            <div className="mb-5 md:mb-6">
               <img
                 src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg"
                 alt="Relume placeholder image 1"
@@ -138,7 +138,7 @@ export function Team6() {
             <p>
               Manages outreach and engagement to promote our research findings.
             </p>
-            <div className="mt-6 grid grid-flow-col grid-cols-[max-content] gap-3.5 self-center">
+            <div className="mt-6 flex gap-3.5">
               <a href="#">
                 <LinkedinLogo className="size-6 text-text-primary" />
               </a>
@@ -150,8 +150,8 @@ export function Team6() {
               </a>
             </div>
           </div>
-          <div className="flex flex-col text-center">
-            <div className="mb-5 flex w-full items-center justify-center md:mb-6">
+          <div className="flex flex-col items-start">
+            <div className="mb-5 md:mb-6">
               <img
                 src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg"
                 alt="Relume placeholder image 1"
@@ -166,7 +166,7 @@ export function Team6() {
               Advises on financial strategies to maximise research funding
               opportunities.
             </p>
-            <div className="mt-6 grid grid-flow-col grid-cols-[max-content] gap-3.5 self-center">
+            <div className="mt-6 flex gap-3.5">
               <a href="#">
                 <LinkedinLogo className="size-6 text-text-primary" />
               </a>
